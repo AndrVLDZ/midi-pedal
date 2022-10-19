f5feccd18e75774544be6251a76f92d66d3e93ea
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2022-10-19 17:33:42 +0300

    fix: fixed the confusion in the names and removed the same scripts

--- MIDI_ch340.ino
+++ MIDI_ch340.ino
@@ -1,9 +1,7 @@
-
 /*
   MIDI message:
   https://www.midi.org/specifications-old/item/table-2-expanded-messages-list-status-bytes
 */
-
 
 void MIDImessage(byte status, byte data1, byte data2)
 {
@@ -11,44 +9,33 @@ void MIDImessage(byte status, byte data1, byte data2)
   Serial.write(data1);
   Serial.write(data2);
 }
-
-
-// POTENTIOMETERS //
-
+// POTENTIOMETER //
 const int pot1 = A0;  // potentiometer pins
 int potVal1 = 0;
 int lastPotVal1 = 0;
 
-
 void readPots() {
-
   int diff = 2; // difference amount
 
-  // READ POTENTIOMETERS //
-  
+  // READ POTENTIOMETER//
   potVal1 = analogRead(pot1);
   // CALCULATE DIFFERENCE BETWEEN NEW VALUE AND LAST RECORDED VALUE //
   
   int potVal1diff = potVal1 - lastPotVal1;
 
   // SEND MIDI MESSAGE //
-  
+ 
   if (abs(potVal1diff) > diff) // execute only if new and old values differ enough
   {
-    MIDImessage(177, 0, map(potVal1, 0, 1023, 0, 127)); // map sensor range to MIDI range
+    MIDImessage(176, 0, map(potVal1, 0, 1023, 0, 127)); // map sensor range to MIDI range
     lastPotVal1 = potVal1; // reset old value with new reading
   }
-  
-  delay(5); // small delay further stabilizes sensor readings
-
+  delay(5); // small delay in milliseconds further stabilizes sensor readings
 }
 
 void setup() {
-
   Serial.begin(9600); // enable serial communication
-  
 }
-
 void loop() {
   readPots();         // read potentiometers
 }
